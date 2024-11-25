9fe59bd93f2125d52d1a08d91e1dbb79027816af
ShawanBarua <124527999+shaw342@users.noreply.github.com>
2024-11-25 20:18:52 +0100

    main.dart

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -6,3 +6,19 @@ Future main() async {
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Flutter app",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      )
+    );
+  }
+}
