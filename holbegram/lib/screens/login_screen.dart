7a5698d3150b88fda6f8fc05d052a5280deeab48
ShawanBarua <124527999+shaw342@users.noreply.github.com>
2024-11-26 20:54:49 +0100

    login_screen

--- holbegram/lib/screens/login_screen.dart
+++ holbegram/lib/screens/login_screen.dart
@@ -1,2 +1,164 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/text_field.dart';
 
+
+
+
+
+class LoginScreen extends StatefulWidget {
+    const LoginScreen({super.key});
+
+    @override
+    State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    bool _passwordVisible = true;
+    
+    @override
+    void initState() {
+        super.initState();
+        _passwordVisible;
+    }
+
+    @override
+    void dispose() {
+        emailController.dispose();
+        passwordController.dispose();
+        super.dispose();
+    }
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.min,
+          children: [
+            SizedBox(height: 28),
+            Text(
+              'Holbegram',
+              style: TextStyle(fontFamily: 'Billabong', fontSize: 50),
+            ),
+            Image.asset(
+              'assets/logo.png', // Remplacez par le chemin de votre logo
+              width: 80,
+              height: 60,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  TextFieldInput(
+                    controller: widget.emailController,
+                    isPassword: false,
+                    hintText: 'Email',
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(height: 24),
+                  TextFieldInput(
+                    controller: widget.passwordController,
+                    isPassword: !_passwordVisible,
+                    hintText: 'Password',
+                    keyboardType: TextInputType.visiblePassword,
+                    suffixIcon: IconButton(
+                      alignment: Alignment.bottomLeft,
+                      icon: Icon(
+                        _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 28),
+            SizedBox(
+              height: 48,
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    Color.fromARGB(218, 226, 37, 24),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Log in',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Forgot your login details?'),
+                Text(
+                  ' Get help logging in',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Flexible(
+              flex: 0,
+              child: Container(),
+            ),
+            SizedBox(height: 24),
+            Divider(thickness: 2),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Sign up',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(218, 226, 37, 24),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Flexible(
+                  child: Divider(thickness: 2),
+                ),
+                Text(' OR '),
+                Flexible(
+                  child: Divider(thickness: 2),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://example.com/google_logo.png', // Remplacez par le lien de votre image
+                  width: 40,
+                  height: 40,
+                ),
+                Text('Sign in with Google'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
